fe5556879272554b234dbe003630f15fad3ee3a0
M.imran <muhammad.imran2@dcmail.ca>
2021-03-12 19:48:31 -0500

    song list added

--- FrontUI_App/music_ai/src/App.js
+++ FrontUI_App/music_ai/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SongTable from './tables/UserRecommendationTable'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Music Recommender App </h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>List of Songs</h2>
+          <SongTable />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ FrontUI_App/music_ai/src/tables/UserRecommendationTable.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const UserRecommendationTable = () => (
+  <table>
+    <thead>
+      <tr>
+        <th>Song Id</th>
+        <th>Song Title</th>
+        <th>Genre</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>  </td>
+      </tr>
+    </tbody>
+  </table>
+)
+
+export default UserRecommendationTable
